95477e2abad53b066977459330831aaa7eb3a333
Morozov Artem <artemmorozov155@gmail.com>
2025-03-08 18:11:52 +0300

    filling the function

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,13 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    int64_t result = 1;
+    while (n > 0) {
+        if (n & 1) {
+            result *= x;
+        }
+        x *= x;
+        n >>= 1;
+    }
+    return result;
 }
