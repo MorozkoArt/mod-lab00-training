75e1b2b261e894c310ca77e0613ab159a13f95fd
Morozov Artem <artemmorozov155@gmail.com>
2025-03-08 23:57:44 +0300

    Several specific cases were considered for a small optimization

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,6 +2,10 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
+    if (n == 0) return 1;
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    if (x == -1) return (n % 2 == 0) ? 1 : -1;
     int64_t result = 1;
     while (n > 0) {
         if (n & 1) {
